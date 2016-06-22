3c5f15b3b0ee89644d199fb52f7e039d93f5c48d
timearchitect <alrik_he@hotmail.com>
2016-06-22 23:56:21 +0200

    controller
    arduino d-pad

---
+++ summer_controller/summer_controller.ino
@@ -0,0 +1,42 @@
+/*--------------------------
+   Foo café
+   Arduino controller
+   Advanced
+   av Alrik He
+  ---------------------------*/
+  
+//Serie kommunications hastighet.
+const int BAUDRATE = 9600;
+
+// piezons pin nummer och ljudfrekvens.
+const int HERTZ = 3100, PIEZO_PIN = 9;
+
+// knapparnas pin nummer.
+const int LEFT_BTN_PIN = 4, RIGHT_BTN_PIN = 5, UP_BTN_PIN = 6, DOWN_BTN_PIN = 7;
+
+void setup() {
+  //knappar i en D-pad format.
+  pinMode(LEFT_BTN_PIN, INPUT_PULLUP);
+  pinMode(RIGHT_BTN_PIN, INPUT_PULLUP);
+  pinMode(UP_BTN_PIN, INPUT_PULLUP);
+  pinMode(DOWN_BTN_PIN, INPUT_PULLUP);
+
+  // högtalare
+  pinMode(PIEZO_PIN, OUTPUT);
+
+  // aktiverar serie kommunication.
+  Serial.begin(BAUDRATE);
+}
+
+void loop() {
+  // ifall någon knapp är tryckt så låter piezo högalaren.
+  if (digitalRead(LEFT_BTN_PIN) == LOW || digitalRead(RIGHT_BTN_PIN) == LOW || digitalRead(UP_BTN_PIN) == LOW || digitalRead(DOWN_BTN_PIN) == LOW) {
+    tone(PIEZO_PIN, HERTZ, 50);
+  }
+
+  // printar ut vilken knapp som är tryck till serie monitorn.
+  Serial.print(digitalRead(LEFT_BTN_PIN));
+  Serial.print(digitalRead(RIGHT_BTN_PIN));
+  Serial.print(digitalRead(UP_BTN_PIN));
+  Serial.println(digitalRead(DOWN_BTN_PIN));
+}
